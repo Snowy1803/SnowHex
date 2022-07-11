f30af22b5959584c35d26ae1a7483d5f657b92ba
Snowy <3mille.prenom.nom@gmail.com>
2022-07-11 21:50:11 +0200

    feat: add total error & warning count in status bar

--- src/st/infos/elementalcube/snowhex/ui/StatusBar.java
+++ src/st/infos/elementalcube/snowhex/ui/StatusBar.java
@@ -9,21 +9,31 @@ import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 
+import st.infos.elementalcube.snowhex.Token.Level;
 import st.infos.elementalcube.snowylangapi.Lang;
 
 public class StatusBar extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 4468240980330665079L;
 	private HexPanel panel;
-	private JLabel colorer, caret, mode;
+	private JLabel colorer, caret, mode, errors, warnings;
 	
 	public StatusBar(HexPanel panel) {
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 		this.panel = panel;
 		add(colorer = new JLabel());
+		add(errors = new JLabel());
+		errors.setIcon(new ImageIcon(StatusBar.class.getResource("/img/error.png")));
+		errors.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+		errors.setVisible(false);
+		add(warnings = new JLabel());
+		warnings.setIcon(new ImageIcon(StatusBar.class.getResource("/img/warning.png")));
+		warnings.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+		warnings.setVisible(false);
 		add(Box.createHorizontalGlue());
 		add(caret = new JLabel());
 		caret.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
@@ -52,5 +62,18 @@ public class StatusBar extends JPanel implements ActionListener {
 		colorer.setText(panel.getColorer() == null ? Lang.getString("parser.none") : panel.getColorer().getLocalizedName());
 		caret.setText(Lang.getString("frame.caretPos", panel.getCaret().getDot() / 16 + 1, panel.getCaret().getDot() % 16));
 		mode.setText(Lang.getString("frame." + (panel.isInsertMode() ? "insert" : "overwrite")));
+
+		if (panel.getTokens() != null) {
+			long errcount = panel.getTokens().stream().filter(t -> t.getToolTipLevel() == Level.ERROR).count();
+			errors.setText("" + errcount);
+			errors.setVisible(errcount > 0);
+		
+			long warncount = panel.getTokens().stream().filter(t -> t.getToolTipLevel() == Level.WARNING).count();
+			warnings.setText("" + warncount);
+			warnings.setVisible(warncount > 0);
+		} else {
+			errors.setVisible(false);
+			warnings.setVisible(false);
+		}
 	}
 }
