c7ac48b108cbb4d84720845d6d0f413c53f5b845
Snowy <3mille.prenom.nom@gmail.com>
2022-07-14 18:49:17 +0200

    feat(png): support IHDR, IDAT and chunk type explanation

--- src/st/infos/elementalcube/snowhex/parser/PNGTokenMaker.java
+++ src/st/infos/elementalcube/snowhex/parser/PNGTokenMaker.java
@@ -38,14 +38,23 @@ public class PNGTokenMaker extends TokenMaker {
 		buf.position(8);
 		buf.order(ByteOrder.BIG_ENDIAN);
 		CRC32 crc = new CRC32();
+		byte[] chunkType = new byte[4];
 		while (true) {
 			int length = buf.getInt();
 			list.add(createToken(TOKEN_LENGTH, buf.position() - 4, 4));
 			crc.reset();
 			crc.update(array, buf.position(), length + 4);
-			int type = buf.getInt(); // let's use ints instead of byte[4]
-			list.add(createToken(TOKEN_CHUNK_HEADER, buf.position() - 4, 4));
-			buf.position(buf.position() + length);
+			buf.get(chunkType);
+			int type = buf.getInt(buf.position() - 4); // use ints instead of byte[4]
+			list.add(createToken(TOKEN_CHUNK_HEADER, buf.position() - 4, 4,
+					notice("chunk", new String(chunkType), 
+							notice("chunk." + ((chunkType[0] & 32) != 0 ? "ancillary" : "critical")), 
+							notice("chunk." + ((chunkType[1] & 32) != 0 ? "private" : "public")), 
+							notice("chunk." + ((chunkType[3] & 32) != 0 ? "safe" : "unsafe"))),
+					Level.INFO));
+			int datastart = buf.position();
+			readChunk(list, buf, type, length);
+			buf.position(datastart + length);
 			int checksum = buf.getInt();
 			if ((int) crc.getValue() != checksum) {
 				list.add(createToken(TOKEN_ERRORED, buf.position() - 4, 4, notice("invalidCrc"), Level.ERROR));
@@ -53,11 +62,51 @@ public class PNGTokenMaker extends TokenMaker {
 				list.add(createToken(TOKEN_RESERVED, buf.position() - 4, 4));
 			}
 			if (type == 0x49_45_4e_44) { // IEND
-				return;
+				return; // last block, skip the rest
 			}
 		}
 	}
 	
+	private void readChunk(ArrayList<Token> list, ByteBuffer buf, int type, int length) {
+		switch (type) {
+		case 0x49_48_44_52: // IHDR
+			int width = buf.getInt();
+			int height = buf.getInt();
+			list.add(createToken(TOKEN_IMAGE_SIZE, buf.position() - 8, 8, notice("size", width, height), Level.INFO));
+			byte bitDepth = buf.get();
+			list.add(createToken(TOKEN_METADATA, buf.position() - 1, 1, notice("bitDepth", bitDepth), Level.INFO));
+			byte colorType = buf.get();
+			if (Arrays.binarySearch(new byte[] {0, 2, 3, 4, 6}, colorType) >= 0) {
+				list.add(createToken(TOKEN_METADATA, buf.position() - 1, 1, notice("colorType", notice("colorType." + colorType)), Level.INFO));
+			} else {
+				list.add(createToken(TOKEN_ERRORED, buf.position() - 1, 1, notice("colorType", notice("colorType.error")), Level.ERROR));	
+			}
+			byte compression = buf.get();
+			if (compression == 0) {
+				list.add(createToken(TOKEN_METADATA, buf.position() - 1, 1, notice("compression." + compression), Level.INFO));
+			} else {
+				list.add(createToken(TOKEN_ERRORED, buf.position() - 1, 1, notice("compression.error"), Level.ERROR));	
+			}
+			byte filter = buf.get();
+			if (filter == 0) {
+				list.add(createToken(TOKEN_METADATA, buf.position() - 1, 1, notice("filter." + filter), Level.INFO));
+			} else {
+				list.add(createToken(TOKEN_ERRORED, buf.position() - 1, 1, notice("filter.error"), Level.ERROR));	
+			}
+			byte interlace = buf.get();
+			if (interlace == 0 || interlace == 1) {
+				list.add(createToken(TOKEN_METADATA, buf.position() - 1, 1, notice("interlace." + interlace), Level.INFO));
+			} else {
+				list.add(createToken(TOKEN_ERRORED, buf.position() - 1, 1, notice("interlace.error"), Level.ERROR));	
+			}
+			break;
+		case 0x49_44_41_54: // IDAT
+			list.add(createToken(TOKEN_IMAGE_DATA, buf.position() - 8, length + 12));
+			list.add(createToken(TOKEN_IMAGE_COLOR, buf.position(), length));
+			break;
+		}
+	}
+
 	static String parseColor(byte[] array, int i) {
 		return getHexString(Integer.toHexString(((array[i++] & 0xFF) << 16) | ((array[i++] & 0xFF) << 8) | ((array[i++] & 0xFF) << 0)), 6);
 	}
